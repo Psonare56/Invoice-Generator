5019bd9ae1633e8c8b6a0d2b7d7059432d41d549
skumar <skumar74604@outlook.com>
2024-06-29 12:46:34 +0530

    Dockerfile

--- Invoicing/Invoicing/settings.py
+++ Invoicing/Invoicing/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'Invoicing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default':{
-    'ENGINE':'django.db.backends.postgresql_psycopg2',
-    'NAME':'invoice',
-    'USER':'invoice_user',
-    'PASSWORD':'invoice_password',
-    'H0ST':'localhost',
-    'PORT':'5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default':{
+#     'ENGINE':'django.db.backends.postgresql_psycopg2',
+#     'NAME':'invoice',
+#     'USER':'invoice_user',
+#     'PASSWORD':'invoice_password',
+#     'H0ST':'localhost',
+#     'PORT':'5432',
+#     }
+# }
 
 
 # Password validation
