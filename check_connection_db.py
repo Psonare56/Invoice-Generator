e55ecfe3d79ff9a86f2594d1c13d6b8a2272aab8
skumar <skumar74604@outlook.com>
2024-06-29 14:10:36 +0530

    script

--- check_connection_db.py
+++ check_connection_db.py
@@ -13,19 +13,25 @@ try:
     # Create a cursor object
     cursor = connection.cursor()
     
-    # Execute a test query
-    cursor.execute("SELECT version(16.3);")
+    # Execute a test query to get the PostgreSQL server version
+    cursor.execute("SELECT version();")
     
     # Fetch the result
     db_version = cursor.fetchone()
-    print(f"Connected to - {16.3}")
+    print(f"Connected to PostgreSQL server version: {db_version[0]}")
 
-except Exception as error:
-    print(f"Error: {error}")
+except psycopg2.Error as e:
+    print(f"Error: Connection to PostgreSQL server failed - {e}")
 
 finally:
     # Close the cursor and connection
-    if connection:
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+    try:
+        if 'connection' in locals():
+            if connection:
+                connection.close()
+                print("PostgreSQL connection is closed")
+        if 'cursor' in locals():
+            if cursor:
+                cursor.close()
+    except psycopg2.Error as e:
+        print(f"Error: Failed to close connection - {e}")
