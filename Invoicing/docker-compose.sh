63b76c4e420cd5e43d0e7c713da3d73f4d7804e8
skumar <skumar74604@outlook.com>
2024-06-30 19:15:47 +0530

    docker-compose

--- Invoicing/docker-compose.sh
+++ Invoicing/docker-compose.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker-compose down --volumes --remove-orphans --rmi all
+docker-compose up --build -d web
 
 
 
 docker-compose logs web
 docker-compose exec web /entrypoint.sh
-docker-compose up --build -d web
