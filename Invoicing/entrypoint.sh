10cda2a9ccb24abfd71e046a1a88b0e11d1b556a
skumar <skumar74604@outlook.com>
2024-06-30 16:12:02 +0530

    Dockerfile

--- Invoicing/entrypoint.sh
+++ Invoicing/entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Create Django superuser
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin') if not User.objects.filter(username='admin').exists() else None"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin') if not User.objects.filter(username='admin').exists() else None"
 
 # Start the Django server
 exec "$@"
