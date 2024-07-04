092e60f8b79f34a274ae10aeb6e5a80f0d115958
skumar <skumar74604@outlook.com>
2024-07-05 02:51:40 +0530

    nginx-docker

--- Invoicing/nginx-docker/wait-for-it.sh
+++ Invoicing/nginx-docker/wait-for-it.sh
@@ -15,17 +15,3 @@ echo "$host:$port is up!"
 
 # Execute the remaining command
 exec "$@"
-
-# # wait-for-it.sh: Wait for a service to become available.
-# # Usage: wait-for-it.sh host:port [cmd] #
-
-# hostport=$1
-# shift
-# cmd="$@"
-
-# while ! nc -z $(echo $hostport | cut -d: -f1) $(echo $hostport | cut -d: -f2); do
-#   echo "Waiting for $hostport..."
-#   sleep 2
-# done
-
-# exec $cmd
