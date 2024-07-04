bec307f5c81133deb23206cc2e629c282983e1b5
skumar <skumar74604@outlook.com>
2024-07-04 22:44:28 +0530

    nginx

--- Invoicing/entrypoint_dockerfile.sh
+++ Invoicing/entrypoint_dockerfile.sh
@@ -3,12 +3,11 @@
 # Apply database migrations
 python manage.py makemigrations
 python manage.py migrate
+python manage.py makemigrations invoice1
 python manage.py migrate invoice1
 
 # Create Django superuser
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin') if not User.objects.filter(username='admin').exists() else None"
 
-# Start the Django server#
+# Start the Django server
 exec "$@"
-
-
